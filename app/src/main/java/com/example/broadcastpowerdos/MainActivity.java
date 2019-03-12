91088578cb6d0d1762b662191f99fbe74d8786a4
ozwaldo <ozwaldo@gmail.com>
2019-03-12 12:42:18 -0600

    12/03/2019

--- app/src/main/java/com/example/broadcastpowerdos/MainActivity.java
+++ app/src/main/java/com/example/broadcastpowerdos/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,24 +19,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    private class CargaBoradcastReceiver extends BroadcastReceiver {
+        tvTexto = (TextView) findViewById(R.id.tvTexto);
+        cargaBoradcastReceiver =
+                new CargaBoradcastReceiver();
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(
+                Intent.ACTION_POWER_CONNECTED);
+        intentFilter.addAction(
+                Intent.ACTION_POWER_DISCONNECTED);
+    }
+    private class CargaBoradcastReceiver extends
+            BroadcastReceiver {
         @Override
-        public void onReceive(Context context, Intent intent) {
+        public void onReceive(Context context,
+                              Intent intent) {
             String action = intent.getAction();
-            boolean cargando = (action.equals(Intent.ACTION_POWER_CONNECTED));
+            boolean cargando = (action.equals(
+                    Intent.ACTION_POWER_CONNECTED));
             showCargando(cargando);
         }
     }
 
     private void showCargando(boolean cargando) {
         if (cargando) {
-
+            tvTexto.setText("Conectado: Cargando corriente.");
+            Toast.makeText(this, "Conectado",
+                    Toast.LENGTH_SHORT).show();
         } else {
 
+            tvTexto.setText(
+                    "Desconectado: Descargando corriente.");
+            Toast.makeText(this, "Desconectado",
+                    Toast.LENGTH_SHORT).show();
         }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(cargaBoradcastReceiver,
+                intentFilter);
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(cargaBoradcastReceiver);
+    }
 }
